e232aff5f14a3c4fcdcc97af5ea67890da18e220
Drew Dara-Abrams <drew@interline.io>
2021-12-02 14:04:46 -0800

    starting map sketch mode

--- src/components/map-viewer.vue
+++ src/components/map-viewer.vue
@@ -3,49 +3,147 @@
     <div id="mapelem" ref="mapelem" :class="mapClass" />
     <div v-if="overlay" class="is-hidden-mobile">
       <div class="map-agencies notification">
-        <div v-if="showOptions" class="map-options">
-          <b-dropdown position="is-bottom-right" append-to-body aria-role="menu" trap-focus>
-            <span
-              slot="trigger"
-              class="navbar-item"
-              role="button"
+        <div class="buttons">
+          <b-switch
+            v-if="showSketchMode"
+            v-model="mode"
+            class="navbar-item is-pulled-left"
+            native-value="explore"
+            true-value="sketch"
+            false-value="explore"
+          >
+            Sketch Mode
+          </b-switch>
+          <span v-if="mode == 'explore' && showExploreOptions" class="is-pulled-right">
+            <!-- OPTIONS FOR EXPLORE MODE -->
+            <b-dropdown
+              position="is-bottom-right"
+              append-to-body
+              aria-role="menu"
+              trap-focus
             >
-              <span>Options</span>
-              <b-icon icon="menu-down" />
-            </span>
+              <template #trigger="{ active }">
+                <b-button
+                  label="Options"
+                  type="is-text"
+                  :icon-right="active ? 'menu-up' : 'menu-down'"
+                />
+              </template>
 
-            <b-dropdown-item
-              aria-role="menu-item"
-              :focusable="false"
-              custom
-              paddingless
-            >
-              <div class="modal-card map-options">
-                <section class="modal-card-body">
-                  <div class="field">
-                    <b-checkbox v-model="showGeneratedShadow">
-                      Show stop-to-stop geometries under 20km
-                    </b-checkbox>
-                  </div>
-                  <div class="field">
-                    <b-checkbox v-model="showLongGeneratedShadow">
-                      Show stop-to-stop geometries over 20km
-                    </b-checkbox>
-                  </div>
-                </section>
-              </div>
-            </b-dropdown-item>
-          </b-dropdown>
+              <b-dropdown-item
+                aria-role="menu-item"
+                :focusable="false"
+                custom
+              >
+                <div class="field">
+                  <b-checkbox v-model="showGeneratedShadow">
+                    Show stop-to-stop geometries under 20km
+                  </b-checkbox>
+                </div>
+                <div class="field">
+                  <b-checkbox v-model="showLongGeneratedShadow">
+                    Show stop-to-stop geometries over 20km
+                  </b-checkbox>
+                </div>
+              </b-dropdown-item>
+            </b-dropdown>
+          </span>
+          <b-button v-if="mode === 'sketch'" type="is-text" class="is-pulled-right" @click="exportSketchGeoJson">
+            Import/Export
+          </b-button>
         </div>
-        <div class="map-info">
+        <div v-if="mode === 'explore'" class="map-info">
           <div v-show="Object.keys(agencyFeatures).length == 0">
             <strong>Use your cursor</strong> to highlight routes and see their names here. <strong>Click</strong> for more details.
           </div>
           <tl-route-select :link="link" :agency-features="agencyFeatures" :collapse="true" />
         </div>
+        <div v-if="mode === 'sketch'" class="map-info">
+          <div v-if="sketchMode === SKETCH_MODES.DRAW_FREEFORM">
+            <p><strong>Click on the map</strong> to sketch a freeform route. Double click on the final point to finish.</p>
+            <div class="buttons has-addons is-right">
+              <b-button type="is-primary is-outlined" @click="cancelSketchingRoute()">
+                Cancel sketching this route
+              </b-button>
+            </div>
+          </div>
+          <div v-else-if="sketchMode === SKETCH_MODES.ROUTE_SELECTED">
+            <b-field label="Name">
+              {{ selectedSketchedRouteGeoJson.id }}
+            </b-field>
+            <div class="buttons has-addons is-right">
+              <b-button type="is-primary is-outlined" @click="unselectSketchedRoute">
+                Unselect
+              </b-button>
+              <b-button type="is-info" @click="snapSelectedSketchedRoute">
+                Snap to roads
+              </b-button>
+              <b-button type="is-danger" @click="deleteSelectedSketchedRoute">
+                Delete
+              </b-button>
+              <b-button type="is-primary" @click="sketchMode = SKETCH_MODES.DRAW_FREEFORM">
+                Sketch another route
+              </b-button>
+            </div>
+          </div>
+          <div v-else-if="sketchMode === SKETCH_MODES.NOTHING_SELECTED">
+            <ul class="content">
+              <li v-for="f in sketchGeoJson.features" :key="f.id">
+                <a @click="selectSketchedRoute(f.id)">{{ f.id }}</a>
+              </li>
+            </ul>
+            <div class="buttons has-addons is-right">
+              <b-button type="is-primary" @click="sketchMode = SKETCH_MODES.DRAW_FREEFORM">
+                Sketch another route
+              </b-button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
+    <!-- sketch GeoJSON modal -->
+    <b-modal
+      v-if="mode === 'sketch'"
+      :active.sync="isSketchGeoJsonModalActive"
+      has-modal-card
+    >
+      <div v-if="isSketchGeoJsonModalActive" class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">
+            Transitland Map Sketch Import/Export
+          </p>
+          <button type="button" class="delete" @click="isSketchGeoJsonModalActive = false" />
+        </header>
+        <section class="modal-card-body">
+          <p class="content">
+            Below is your sketch in GeoJSON format. Copy the contents and save on your computer for future use. Or post it to a GitHub Gist for sharing.
+          </p>
+          <p class="content">
+            To import an existing sketch, paste GeoJSON into this form and press <em>Import</em>.
+          </p>
+          <b-notification v-if="sketchGeoJsonError" type="is-danger">
+            {{ sketchGeoJsonError }}
+          </b-notification>
+          <textarea v-model="sketchGeoJsonString" class="textarea" style="width: 100%; height: 80%; min-height: 300px;" />
+        </section>
+        <footer class="modal-card-foot">
+          <div class="buttons is-right">
+            <b-button
+              label="Close"
+              @click="isSketchGeoJsonModalActive = false"
+            />
+            <b-button
+              label="Import"
+              type="is-primary"
+              @click="importSketchGeoJson"
+            />
+          </div>
+        </footer>
+      </div>
+    </b-modal>
+
+    <!-- explore modal -->
     <b-modal
       v-if="overlay && link"
       :active.sync="isComponentModalActive"
@@ -71,11 +169,13 @@
 
 <script>
 import maplibre from 'maplibre-gl'
+import MapboxDraw from '@mapbox/mapbox-gl-draw'
 import mapLayers from './map-layers.js'
 
 export default {
   props: {
-    showOptions: { type: Boolean, default: false },
+    showExploreOptions: { type: Boolean, default: false },
+    showSketchMode: { type: Boolean, default: false },
     showGenerated: { type: Boolean, default: true },
     showLongGenerated: { type: Boolean, default: true },
     mapClass: { type: String, default: 'short' },
@@ -99,7 +199,15 @@ export default {
   },
   data () {
     return {
+      mode: 'explore',
+      sketchMode: null,
+      isSketchGeoJsonModalActive: false,
+      sketchGeoJson: {},
+      sketchGeoJsonString: null,
+      sketchGeoJsonError: null,
+      selectedSketchedRouteGeoJson: null,
       map: null,
+      drawControl: null,
       hovering: [],
       agencyFeatures: {},
       isComponentModalActive: false,
@@ -120,6 +228,109 @@ export default {
           this.updateFeatures()
         })
       }
+    },
+    mode (v) {
+      if (v === 'sketch') {
+        if (this.drawControl === null) {
+          this.drawControl = new MapboxDraw({
+            displayControlsDefault: false,
+            styles: [
+            // ACTIVE (being drawn)
+            // line stroke
+              {
+                id: 'gl-draw-line',
+                type: 'line',
+                filter: ['all', ['==', '$type', 'LineString'], ['==', 'active', 'true']],
+                layout: {
+                  'line-cap': 'round',
+                  'line-join': 'round'
+                },
+                paint: {
+                  'line-color': '#D20C0C',
+                  'line-width': 8
+                }
+              },
+              // vertex point halos
+              {
+                id: 'gl-draw-polygon-and-line-vertex-halo-active',
+                type: 'circle',
+                filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
+                paint: {
+                  'circle-radius': 10,
+                  'circle-color': '#FFF'
+                }
+              },
+              // vertex points
+              {
+                id: 'gl-draw-polygon-and-line-vertex-active',
+                type: 'circle',
+                filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
+                paint: {
+                  'circle-radius': 8,
+                  'circle-color': '#D20C0C'
+                }
+              },
+
+              // INACTIVE (static, already drawn)
+              // line stroke
+              {
+                id: 'gl-draw-line-static',
+                type: 'line',
+                filter: ['all', ['==', '$type', 'LineString'], ['!=', 'active', 'true']],
+                layout: {
+                  'line-cap': 'round',
+                  'line-join': 'round'
+                },
+                paint: {
+                  'line-color': '#000',
+                  'line-width': 8
+                }
+              }
+            ]
+          })
+          this.map.addControl(this.drawControl)
+          // recover saved GeoJSON
+          const retrivedGeoJsonString = localStorage.getItem(this.TRANSITLAND_SKETCH_LS)
+          if (retrivedGeoJsonString) {
+            try {
+              const parsedRetrievedGeoJson = JSON.parse(retrivedGeoJsonString)
+              if (parsedRetrievedGeoJson) {
+                this.drawControl.add(parsedRetrievedGeoJson)
+              }
+            } catch (e) {
+              alert('Found a saved sketch, but its GeoJSON is corrupt. Starting over.')
+              localStorage.removeItem(this.TRANSITLAND_SKETCH_LS)
+            }
+          }
+        }
+        this.map.on('draw.modechange', this.mapWantsToChangeSketchMode)
+        this.map.on('draw.selectionchange', this.mapWantsToChangeSketchMode)
+        if (this.sketchGeoJson?.features?.length > 0) {
+          // when returning to an existing sketch, start with nothing selected
+          this.sketchMode = this.SKETCH_MODES.NOTHING_SELECTED
+        } else {
+          // when starting a new sketch, immediately start to draw first route
+          this.sketchMode = this.SKETCH_MODES.DRAW_FREEFORM
+        }
+      } else if (v === 'explore') {
+        this.unselectSketchedRoute()
+        this.map.off('draw.modechange', this.mapWantsToChangeSketchMode)
+        this.map.off('draw.selectionchange', this.mapWantsToChangeSketchMode)
+      }
+    },
+    sketchMode (v) {
+      if (v === this.SKETCH_MODES.DRAW_FREEFORM) {
+        this.drawControl.changeMode('draw_line_string')
+      } else if (v === this.SKETCH_MODES.ROUTE_SELECTED) {
+        // TODO
+      }
+    },
+    sketchGeoJsonString (v) {
+      if (v && JSON.parse(v)) {
+        this.sketchGeoJson = JSON.parse(v)
+      } else {
+        this.sketchGeoJsonError = 'Error parsing GeoJSON.'
+      }
     }
   },
   mounted () {
@@ -127,7 +338,62 @@ export default {
       this.initMap()
     }
   },
+  created () {
+    this.SKETCH_MODES = {
+      NOTHING_SELECTED: 'nothing_selected',
+      DRAW_FREEFORM: 'draw_freeform',
+      ROUTE_SELECTED: 'route_selected'
+    }
+    this.TRANSITLAND_SKETCH_LS = 'transitland_sketch_ls'
+  },
   methods: {
+    /* SKETCH METHODS */
+    mapWantsToChangeSketchMode (e) {
+      if (e.type === 'draw.selectionchange' && e.features.length === 1) {
+        // TODO: handle multiple select (when shift is pressed) or disable it
+        this.selectedSketchedRouteGeoJson = e.features[0]
+        this.sketchMode = this.SKETCH_MODES.ROUTE_SELECTED
+      } else if (e.type === 'draw.selectionchange' && e.features.length === 0) {
+        this.sketchMode = this.SKETCH_MODES.NOTHING_SELECTED
+      } else {
+        console.log(e)
+      }
+      this.updateSketchGeoJson()
+    },
+    updateSketchGeoJson () {
+      this.sketchGeoJson = this.drawControl.getAll()
+      localStorage.setItem(this.TRANSITLAND_SKETCH_LS, JSON.stringify(this.sketchGeoJson))
+    },
+    unselectSketchedRoute () {
+      this.drawControl.changeMode('simple_select')
+      this.sketchMode = this.SKETCH_MODES.NOTHING_SELECTED
+    },
+    snapSelectedSketchedRoute () {
+      alert('TODO')
+    },
+    deleteSelectedSketchedRoute () {
+      this.drawControl.delete(this.selectedSketchedRouteGeoJson.id)
+      this.updateSketchGeoJson()
+      this.unselectSketchedRoute()
+    },
+    selectSketchedRoute (id) {
+      this.drawControl.changeMode('direct_select', { featureId: id })
+      // this.selectedSketchedRouteGeoJson = this.sketchGeoJson.features.find(f => f.id === id)
+      this.sketchMode = this.SKETCH_MODES.ROUTE_SELECTED
+    },
+    cancelSketchingRoute () {
+      this.drawControl.trash()
+      this.sketchMode = this.SKETCH_MODES.NOTHING_SELECTED
+    },
+    exportSketchGeoJson () {
+      this.sketchGeoJsonString = JSON.stringify(this.sketchGeoJson, null, 2)
+      this.isSketchGeoJsonModalActive = true
+    },
+    importSketchGeoJson () {
+      this.drawControl.set(this.sketchGeoJson)
+      this.isSketchGeoJsonModalActive = false
+    },
+    /* EXPLORE METHODS */
     saveImage () {
       const canvas = this.map.getCanvas() // .toDataURL('image/png')
       const fileName = 'image'
@@ -384,33 +650,45 @@ export default {
       }
     },
     mapClick (e) {
-      this.isComponentModalActive = true
+      if (this.mode === 'explore') {
+        this.isComponentModalActive = true
+      }
     },
     mapMouseMove (e) {
-      const map = this.map
-      const features = map.queryRenderedFeatures(e.point, { layers: ['route-active'] })
-      map.getCanvas().style.cursor = 'pointer'
-      for (const k of this.hovering) {
-        map.setFeatureState(
-          { source: 'routes', id: k, sourceLayer: this.routeTiles ? this.routeTiles.id : null },
-          { hover: false }
-        )
-      }
-      this.hovering = []
-      for (const v of features) {
-        this.hovering.push(v.id)
-        map.setFeatureState({ source: 'routes', id: v.id, sourceLayer: this.routeTiles ? this.routeTiles.id : null }, { hover: true })
-      }
-      const agencyFeatures = {}
-      for (const v of features) {
-        const agencyId = v.properties.agency_name
-        const routeId = v.properties.route_id
-        if (agencyFeatures[agencyId] == null) {
-          agencyFeatures[agencyId] = {}
+      if (this.mode === 'explore') {
+        const map = this.map
+
+        const stops = map.queryRenderedFeatures(e.point, { layers: ['stops'] })
+        console.log(stops)
+        for (const s of stops) {
+          map.setFeatureState({ source: 'stops', id: s.id, sourceLayer: this.stopTiles ? this.stopTiles.id : null }, { hover: true })
         }
-        agencyFeatures[agencyId][routeId] = v.properties
+
+
+        const routes = map.queryRenderedFeatures(e.point, { layers: ['route-active'] })
+        map.getCanvas().style.cursor = 'pointer'
+        for (const k of this.hovering) {
+          map.setFeatureState(
+            { source: 'routes', id: k, sourceLayer: this.routeTiles ? this.routeTiles.id : null },
+            { hover: false }
+          )
+        }
+        this.hovering = []
+        for (const v of routes) {
+          this.hovering.push(v.id)
+          map.setFeatureState({ source: 'routes', id: v.id, sourceLayer: this.routeTiles ? this.routeTiles.id : null }, { hover: true })
+        }
+        const agencyFeatures = {}
+        for (const v of routes) {
+          const agencyId = v.properties.agency_name
+          const routeId = v.properties.route_id
+          if (agencyFeatures[agencyId] == null) {
+            agencyFeatures[agencyId] = {}
+          }
+          agencyFeatures[agencyId][routeId] = v.properties
+        }
+        this.agencyFeatures = agencyFeatures
       }
-      this.agencyFeatures = agencyFeatures
     }
   }
 }
@@ -418,6 +696,7 @@ export default {
 
 <style scss>
 @import 'maplibre-gl/dist/maplibre-gl';
+@import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 
 .short {
   height: 600px;
@@ -435,8 +714,9 @@ export default {
   top:10px;
   left:10px;
   background:#ffffff;
+  border: 1px solid gray;
   width:400px;
-  opacity:0.5;
+  opacity:1;
 }
 
 .map-options {
