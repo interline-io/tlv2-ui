b3ab1d36388e21f73cdfc1b4a167fabbf2e79c3d
Ian Rees <ian@ianrees.net>
2022-05-04 15:02:40 -0700

    Enum updates

--- src/components/pages/feeds.vue
+++ src/components/pages/feeds.vue
@@ -41,13 +41,13 @@
             <option value="">
               All
             </option>
-            <option value="success">
+            <option value="SUCCESS">
               Success
             </option>
-            <option value="error">
+            <option value="ERROR">
               Error
             </option>
-            <option value="in_progress">
+            <option value="IN_PROGRESS">
               In progress
             </option>
           </b-select>
@@ -63,16 +63,16 @@
 
         <b-field label="Filter by data format" class="pl-3">
           <div class="pt-2">
-            <b-checkbox v-model="feedSpecs" native-value="gtfs" class="is-medium">
+            <b-checkbox v-model="feedSpecs" native-value="GTFS" class="is-medium">
               <abbr title="General Transit Feed Specification">GTFS</abbr>
             </b-checkbox>
-            <b-checkbox v-model="feedSpecs" native-value="gtfs-rt" class="is-medium">
+            <b-checkbox v-model="feedSpecs" native-value="GTFS_RT" class="is-medium">
               <abbr title="GTFS Realtime">GTFS-RT</abbr>
             </b-checkbox>
-            <b-checkbox v-model="feedSpecs" native-value="gbfs" class="is-medium">
+            <b-checkbox v-model="feedSpecs" native-value="GBFS" class="is-medium">
               <abbr title="General Bikeshare Feed Specification">GBFS</abbr>
             </b-checkbox>
-            <b-checkbox v-model="feedSpecs" native-value="mds" class="is-medium">
+            <b-checkbox v-model="feedSpecs" native-value="MDS" class="is-medium">
               <abbr title="Mobility Data Specification">MDS</abbr>
             </b-checkbox>
           </div>
@@ -217,7 +217,7 @@ export default {
     } else if (spec) {
       spec = [spec]
     } else {
-      spec = ['gtfs', 'gtfs-rt', 'gbfs', 'mds']
+      spec = ['GTFS', 'GTFS_RT', 'GBFS', 'MDS']
     }
     return {
       feedSpecs: spec,
