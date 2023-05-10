3a7b12becef4e335ad9edc5f6dc8eb0880a1fa6a
Ian Rees <ian@ianrees.net>
2023-05-10 14:35:14 -0700

    Fix search bar update event

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -1,36 +1,36 @@
 <template>
-      <o-input
-        expanded
-        root-class="is-expanded ml-0 mr-2"
-        :value="modelValue"
-        :placeholder="modelValue ? modelValue : placeholder"
-        type="text"
-        icon="magnify"
-        :icon-right="modelValue ? 'close-circle' : null"
-        icon-right-clickable
-        class="entity-search"
-        @input="$emit('update:modelValue', $event.target.value)"
-        @icon-right-click="clearSearch"
-        @keydown.esc="clearSearch"
-      />
+  <o-input
+    expanded
+    root-class="is-expanded ml-0 mr-2"
+    :value="modelValue"
+    :placeholder="modelValue ? modelValue : placeholder"
+    type="text"
+    icon="magnify"
+    :icon-right="modelValue ? 'close-circle' : null"
+    icon-right-clickable
+    class="entity-search"
+    @update:model-value="$emit('update:modelValue', $event)"
+    @icon-right-click="clearSearch"
+    @keydown.esc="clearSearch"
+  />
 </template>
 <script>
 export default {
   props: {
-    modelValue: { type: String, default: "" },
-    placeholder: { type: String, default: "Search" },
+    modelValue: { type: String, default: '' },
+    placeholder: { type: String, default: 'Search' }
   },
-  emits: ["update:modelValue"],
+  emits: ['update:modelValue'],
   methods: {
     clearSearch() {
-      this.$emit("update:modelValue", "");
-    },
-  },
-};
+      this.$emit('update:modelValue', '')
+    }
+  }
+}
 </script>
 
 <style scoped>
 .entity-search {
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 }
 </style>
