25b74941494e69b52beef99f88e325a60ce53939
Drew Dara-Abrams <drew@interline.io>
2023-09-14 20:40:53 -0700

    styling improvements

--- src/components/user-icon.vue
+++ src/components/user-icon.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>
-    <span v-if="user && user.id" class="button is-primary" @click="logout">Logout {{ user.id.slice(0,10) }}...</span>
-    <span v-else class="button is-primary" @click="login">Login</span>
-  </div>
+  <template v-if="user && user.id">
+    <div class="navbar-item">
+      <em>You're browsing as <strong>{{ user.id.slice(0,10) }}</strong></em> <!-- TODO: replace with user's name and/or email -->
+    </div>
+    <div class="navbar-item">
+      <span class="button is-primary" @click="logout">Sign out</span>
+    </div>
+  </template>
+  <template v-else>
+    <div class="navbar-item">
+      <em>Unlock more functionality with a free account:</em>
+    </div>
+    <div class="navbar-item">
+      <div class="field has-addons">
+        <span class="button is-primary" @click="login">Sign in</span>
+        <a href="https://app.interline.io/products/tlv2_api/orders/new" class="button is-secondary">Sign up</a>
+      </div>
+    </div>
+  </template>
 </template>
 
 <script>
