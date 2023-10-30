70d0158d67a67e2bfe5b15fe8ec9fc00e4130be4
Ian Rees <ian@ianrees.net>
2023-10-30 16:49:15 -0700

    Check roles in login-gate

--- src/runtime/components/login-gate.vue
+++ src/runtime/components/login-gate.vue
@@ -1,20 +1,38 @@
 <template>
-  <div>
-    <div v-if="useLoginGate">
-      <slot name="loginText">
-        Login required
-      </slot>
+  <client-only>
+    <div v-if="notOk">
+      <div v-if="loggedIn">
+        <slot name="roleText">
+          Feature unavailable
+        </slot>
+      </div>
+      <div v-else>
+        <slot name="loginText">
+          Login required
+        </slot>
+      </div>
     </div>
     <div v-else>
       <slot name="default" />
     </div>
-  </div>
+  </client-only>
 </template>
 
 <script>
+import { useUser } from '../plugins/auth'
+import { useLoginGate } from '../composables/useLoginGate'
+
 export default {
+  props: {
+    role: { type: String, default: null }
+  },
   data () {
-    return { useLoginGate: useLoginGate() }
+    const notOk = useLoginGate(this.role)
+    const loggedIn = useUser()?.loggedIn
+    return {
+      notOk,
+      loggedIn
+    }
   }
 }
 </script>
