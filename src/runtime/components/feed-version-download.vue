aac5de2dd2a421d6a5f249f4402bb1e0fccc5ddb
Ian Rees <ian@ianrees.net>
2024-08-13 16:16:05 -0700

    Improvements to feed version downloader (#111)

--- src/runtime/components/feed-version-download.vue
+++ src/runtime/components/feed-version-download.vue
@@ -1,5 +1,15 @@
 <template>
-  <span class="button is-primary" @click="download">Download</span>
+  <div>
+    <tl-msg-error v-if="error">
+      {{ error }}
+    </tl-msg-error>
+    <o-button v-else-if="loading" disabled>
+      Downloading... please wait.
+    </o-button>
+    <o-button v-else variant="success" icon-left="download" @click="download">
+      {{ text }}
+    </o-button>
+  </div>
 </template>
 
 <script>
@@ -8,14 +18,20 @@ import Loadable from './loadable'
 export default {
   mixins: [Loadable],
   props: {
+    text: { type: String, default: 'Download' },
     feedOnestopId: { type: String, required: true },
-    feedVersionSha1: { type: String, required: true }
+    feedVersionSha1: { type: String, default: '' },
+    latest: { type: Boolean, default: false }
   },
   methods: {
     async download() {
+      this.loading = true
       console.log('download')
+      const url = this.latest
+        ? `${this.apiBase}/rest/feeds/${this.feedOnestopId}/download_latest_feed_version`
+        : `${this.apiBase}/rest/feed_versions/${this.feedVersionSha1}/download`
       let filename = ''
-      await fetch(`${this.apiBase}/rest/feed_versions/${this.feedVersionSha1}/download`, {
+      await fetch(url, {
         headers: { authorization: await this.authBearer() }
       })
         .then((result) => {
@@ -28,7 +44,9 @@ export default {
           return result.blob()
         })
         .then((blob) => {
+          this.loading = false
           if (blob != null) {
+            console.log('download: got blob')
             const url = window.URL.createObjectURL(blob)
             const a = document.createElement('a')
             a.href = url
@@ -39,8 +57,9 @@ export default {
           }
         })
         .catch((err) => {
-          console.log(err)
-          alert(err)
+          console.log('download: failed', err)
+          this.loading = false
+          this.error = err
         })
     }
   }
