90a1844ffc508dddeb906ea299e579456a35202f
Ian Rees <ian@ianrees.net>
2024-10-09 15:22:52 -0700

    Try 2

--- src/runtime/plugins/oruga.ts
+++ src/runtime/plugins/oruga.ts
@@ -1,9 +1,10 @@
-import { bulmaConfig } from '@oruga-ui/theme-bulma'
 import '@mdi/font/css/materialdesignicons.min.css'
 
 // sigh
 import Oruga from '@oruga-ui/oruga-next'
+import '@oruga-ui/theme-oruga/dist/oruga.css'
 
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
 import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
