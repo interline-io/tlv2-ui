e50cabe90f65c2709fe0aa53a84ddf610c138db2
Drew Dara-Abrams <drew@interline.io>
2025-03-06 16:41:16 -0800

    fix import

--- src/runtime/plugins/mixpanel.client.ts
+++ src/runtime/plugins/mixpanel.client.ts
@@ -1,4 +1,4 @@
-import { mixpanel } from 'mixpanel-browser'
+import mixpanel from 'mixpanel-browser'
 import { defineNuxtPlugin, useRuntimeConfig } from '#imports'
 import { useUser } from './auth'
 
