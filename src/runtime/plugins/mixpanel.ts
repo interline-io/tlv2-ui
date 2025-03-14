06978ded1f3706867013d69234ac8ae94e4409f9
Ian Rees <ian@ianrees.net>
2025-03-14 01:54:00 -0700

    Mixpanel debug

--- src/runtime/plugins/mixpanel.ts
+++ src/runtime/plugins/mixpanel.ts
@@ -1,48 +1,83 @@
-//import { mixpanel } from 'mixpanel-browser'
-// import { useUser } from './auth'
-// import { useRuntimeConfig } from '#imports'
+import mixpanel from 'mixpanel-browser'
+import { defineNuxtPlugin, useRuntimeConfig } from '#imports'
+import { useUser } from './auth'
+
+interface User {
+  id: string
+  email: string
+  name: string
+}
+
+interface MixpanelInstance {
+  track: (msg: string, args: any) => void
+  identify: (properties?: Record<string, any>) => void
+  reset: () => void
+}
 
 // Mixpanel init
 let init = false
 let hasUser = false
 
-export const useMixpanel = () => {
-  if (process.server) {
-    return
+const createMixpanel = (): MixpanelInstance => {
+  const config = useRuntimeConfig()
+  if (!config.public.mixpanelApikey) {
+    return { 
+      track: (msg: string, args: any) => { console.log('mixpanel dummy track (no API key):', msg, args) },
+      identify: () => {},
+      reset: () => {}
+    }
   }
-  return { track: (msg: string, args: any) => { console.log('mixpanel dummy track:', msg, args) } }
-  // const config = useRuntimeConfig()
-  // if (!config.public.mixpanelApikey) {
-  //   // console.log('mixpanel: no config')
-  //   return { track: (msg: string, args: any) => { console.log('mixpanel dummy track:', msg, args) } }
-  // }
-  // if (!init) {
-  //   console.log('mixpanel: init')
-  //   mixpanel.init(config.public.mixpanelApikey, {
-  //     debug: true,
-  //     persistence: 'localStorage',
-  //     ignore_dnt: true // to also log Drew's reports
-  //   })
-  //   init = true
-  // }
-  // if (!hasUser) {
-  //   const user = useUser()
-  //   if (user && user.id) {
-  //     const mpUser = {
-  //       $email: user.email,
-  //       $name: user.name
-  //       // $groups: (user.groups || []).map((s) => { return s.name })
-  //     }
-  //     console.log('mixpanel: identify', mpUser)
-  //     mixpanel.identify(user.id)
-  //     mixpanel.people.set(mpUser)
-  //     hasUser = true
-  //   }
-  // }
-  // return {
-  //   track: (msg: string, args: any) => {
-  //     console.log('mixpanel track:', msg, args)
-  //     mixpanel.track(msg, args)
-  //   }
-  // }
+
+  if (!init) {
+    console.log('mixpanel: init')
+    mixpanel.init(config.public.mixpanelApikey, {
+      debug: true,
+      persistence: 'localStorage',
+      ignore_dnt: true
+    })
+    init = true
+  }
+
+  return {
+    track: (msg: string, args: any) => {
+      console.log('mixpanel track:', msg, args)
+      mixpanel.track(msg, args)
+    },
+    identify: (properties?: Record<string, any>) => {
+      if (hasUser) return
+      const user = useUser() as User
+      if (user && user.id) {
+        const mpUser: any = {
+          $email: user.email,
+          $name: user.name,
+          ...properties
+        }
+        console.log('mixpanel: identify', mpUser)
+        mixpanel.identify(user.id)
+        mixpanel.people.set(mpUser)
+        hasUser = true
+      }
+    },
+    reset: () => {
+      console.log('mixpanel: reset')
+      hasUser = false
+      mixpanel.reset()
+    }
+  }
 }
+
+export default defineNuxtPlugin(() => {
+  const mp = createMixpanel()
+  return {
+    provide: {
+      mixpanel: mp
+    }
+  }
+})
+
+// Composable for use in components
+export const useMixpanel = (): MixpanelInstance => {
+  const mp = createMixpanel()
+  mp.identify()
+  return mp
+} 
