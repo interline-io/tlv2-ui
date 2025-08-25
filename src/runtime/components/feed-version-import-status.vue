041cc2cdb13fa2083d6d8c3aa85fb88b66bebc63
Drew Dara-Abrams <drew@interline.io>
2025-08-25 12:46:33 -0700

    feed version service coverage timeline chart (#185)
    * feed version service coverage timeline
    
    * move service levels tab to the start of fv detail page
    
    * add import/active status to fv detail page
    
    * import/active status
    
    * separate tabs
    
    * text

---
+++ src/runtime/components/feed-version-import-status.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex items-center gap-2">
+    <template v-if="feedVersionGtfsImport">
+      <o-tooltip
+        v-if="feedVersionGtfsImport.schedule_removed"
+        label="Agencies, stops, and routes available"
+      >
+        <o-icon icon="check" />
+      </o-tooltip>
+      <o-tooltip v-else-if="feedVersionGtfsImport.success" label="Successfully imported and available through Transitland APIs">
+        <o-icon icon="check-all" />
+      </o-tooltip>
+      <o-tooltip v-else-if="feedVersionGtfsImport.in_progress" label="Import in progress">
+        <o-icon icon="clock" />
+      </o-tooltip>
+      <o-tooltip
+        v-else-if="feedVersionGtfsImport.success == false"
+        :label="`Not available through Transitland REST and GraphQL APIs:${feedVersionGtfsImport.exception_log.join('\n')}`"
+        position="top"
+      >
+        <o-icon icon="alert" />
+      </o-tooltip>
+    </template>
+    <template v-else-if="showNotImportedStatus">
+      <o-tooltip label="Not currently imported into API. Still available for download from the Feed Archive.">
+        <o-icon icon="close" class="text-danger" />
+      </o-tooltip>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    feedVersionGtfsImport: {
+      type: Object,
+      default: null
+    },
+    showNotImportedStatus: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
