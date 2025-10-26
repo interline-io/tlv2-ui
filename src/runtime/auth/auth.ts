55eeddf91d5daa6134737dae08811b2fd3a4b6a9
Ian Rees <ian@ianrees.net>
2025-10-26 00:17:40 -0700

    Include cytoscape-fcose and cytoscape in optimizedeps (#220)
    * Include cytoscape-fcose and cytoscape in optimizedeps
    
    * Start expanding login-gate
    
    * Login gate improvements
    
    * Fix bug in logingate
    
    * Remove debug

--- src/runtime/auth/auth.ts
+++ src/runtime/auth/auth.ts
@@ -77,22 +77,50 @@ export const useLogout = async () => {
 // User
 /// ////////////////////
 
-export const useLoginGate = (role?: string): boolean => {
-  // console.log('useLoginGate')
+export const useLoginGate = (options: { hasRole?: string, hasAnyRole?: string[], excludeAnyRole?: string[] }): boolean => {
+  // Returns TRUE if GATE REQUIRED
+  // Get config
   const config = useRuntimeConfig()
-  if (config.public.tlv2?.loginGate) {
-    // console.log('useLoginGate: config is true')
-    const user = useUser()
-    if (user?.loggedIn) {
-      // console.log('user??', user, 'role:', role, 'has role:', user.hasRole(role))
-      // console.log('useLoginGate: user is logged in')
-      if (role) {
-        return !user.hasRole(role)
-      }
-      return false
-    }
-    // console.log('useLoginGate: user not logged in, login required')
+  if (!config.public.tlv2?.loginGate) {
+    // Login gate disabled: always authorized
+    console.log('useLoginGate: login gate disabled')
+    return false
+  }
+  // Get user
+  const user = useUser()
+  if (!user.loggedIn) {
+    // Not logged in: not authorized
+    console.log('useLoginGate: not logged in')
     return true
   }
-  return false
+
+  // Combine options
+  let { hasRole, hasAnyRole, excludeAnyRole } = options
+  const hasRoleCopy = [...hasAnyRole || []]
+  if (hasRole) {
+    hasRoleCopy.push(hasRole)
+  }
+  console.log('useLoginGate: hasAnyRole:', hasRoleCopy)
+  console.log('useLoginGate: excludeAnyRole:', excludeAnyRole)
+
+  // Check exclusions and roles
+  for (const excludeRole of excludeAnyRole || []) {
+    if (user.hasRole(excludeRole)) {
+      // Has an excluded role: not authorized
+      return true
+    }
+  }
+
+  if (hasRoleCopy.length === 0) {
+    // No roles required: authorized
+    return false
+  }
+
+  for (const r of hasRoleCopy || []) {
+    if (user.hasRole(r)) {
+      // Has one of the hasAnyRole: authorized
+      return false
+    }
+  }
+  return true
 }
