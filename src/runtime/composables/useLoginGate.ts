a47e613acc899ffb905a0ecf1c545d389d4ed9c2
Ian Rees <ian@ianrees.net>
2025-10-27 18:59:55 -0700

    Debug useLoginGate

--- src/runtime/composables/useLoginGate.ts
+++ src/runtime/composables/useLoginGate.ts
@@ -11,14 +11,14 @@ export const useLoginGate = (options: { hasRole?: string, hasAnyRole?: string[],
   const config = useRuntimeConfig()
   if (!config.public.tlv2?.loginGate) {
     // Login gate disabled: always authorized
-    console.log('useLoginGate: login gate disabled')
+    console.log('useLoginGate: (not gated) login gate disabled')
     return false
   }
   // Get user
   const user = useUser()
   if (!user.loggedIn) {
     // Not logged in: not authorized
-    console.log('useLoginGate: not logged in')
+    console.log('useLoginGate: (gated) not logged in')
     return true
   }
 
@@ -33,20 +33,25 @@ export const useLoginGate = (options: { hasRole?: string, hasAnyRole?: string[],
   for (const excludeRole of excludeAnyRole || []) {
     if (user.hasRole(excludeRole)) {
       // Has an excluded role: not authorized
+      console.log('useLoginGate: (gated) has excluded role', excludeRole)
       return true
     }
   }
 
   if (hasRoleCopy.length === 0) {
     // No roles required: authorized
+    console.log('useLoginGate: (not gated) no roles required')
     return false
   }
 
   for (const r of hasRoleCopy || []) {
     if (user.hasRole(r)) {
       // Has one of the hasAnyRole: authorized
+      console.log('useLoginGate: (not gated) user has required role', r)
       return false
     }
   }
+  // Does not have any of the required roles: not authorized
+  console.log('useLoginGate: (gated) user does not have any of the required roles', hasRoleCopy)
   return true
 }
